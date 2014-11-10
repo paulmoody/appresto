40833e9fda0a10b2cfd1bad59bfdd01636f2051b
paulmoody <moodyweb@hotmail.com>
2014-11-10 16:11:52 -0430

    cambio check
    cambio de imagen a 25

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ var app = {
     	    	for(i = 0; i<arr.length; i++) 
     	    	{
     	    		e = arr[i];
-    	    		$('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.email  + '</a><img src="img/check40.png"></li>');
+    	    		$('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.email  + '</a><img src="img/check25.png"></li>');
     	    	}
 	},
 	
