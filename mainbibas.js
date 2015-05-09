2882f7b67f4e91696f657b41a6d69e52cd6219f1
paulmoody <moodyweb@hotmail.com>
2015-05-09 11:40:11 -0430

    carga de prueba

--- mainbibas.js
+++ mainbibas.js
@@ -1,5 +1,35 @@
 var app = {
 
+	//--------------------------------------------------------------
+	
+	var rootURL = "http://localhost/rest/api/wines";
+	
+	//findAll();
+	
+	function findAll() {
+		console.log('findAll');
+		$.ajax({
+			type: 'GET',
+			url: rootURL,
+			dataType: "json", // data type of response
+			success: renderList
+		});
+	}
+
+	
+	function renderList(data) {
+		// JAX-RS serializes an empty list as null, and a 'collection of one' as an object (not an 'array of one')
+		var list = data == null ? [] : (data.wine instanceof Array ? data.wine : [data.wine]);
+	
+		$('#wineList li').remove();
+		$.each(list, function(index, wine) {
+			$('#wineList').append('<li><a href="#" data-identity="' + wine.product_id + '">'+wine.name+'</a></li>');
+		});
+	}
+
+	
+	//------------------------------------------------------------------
+	
 	
 	dibujarRegistrosParam: function(arr) {
     	    	var out = "";
@@ -22,7 +52,8 @@ var app = {
     initialize: function() {
     	this.homeTpl = Handlebars.compile($("#home-tpl").html());
     	this.renderHomeView();    	
-    	this.dibujarRegistrosParam(registrosJson);
+    	//this.dibujarRegistrosParam(registrosJson);
+    	findAll();
 	}
 };
 
