5717e725dd17756dec8054dd7e3ac4f8c17b3b2c
paulmoody <moodyweb@hotmail.com>
2015-05-13 04:07:06 -0430

    data storage locas

--- mainbibas.js
+++ mainbibas.js
@@ -1,127 +1,34 @@
 var app = {
 
-	//--------------------------------------------------------------
-	
-	//var rootURL = "http://localhost/rest/api/wines";
-	
-	/*findAll();
-	
-	function findAll() {
-		console.log('findAll');
-		$.ajax({
-			type: 'GET',
-			url: "http://localhost/rest/api/wines",
-			dataType: "json", // data type of response
-			success: renderList
-		});
-	}
+					 
 
-	
-	function renderList(data) {
-		// JAX-RS serializes an empty list as null, and a 'collection of one' as an object (not an 'array of one')
-		var list = data == null ? [] : (data.wine instanceof Array ? data.wine : [data.wine]);
-	
-		$('#wineList li').remove();
-		$.each(list, function(index, wine) {
-			$('#wineList').append('<li><a href="#" data-identity="' + wine.product_id + '">'+wine.name+'</a></li>');
-		});
-	}
-
-	*/
-	//------------------------------------------------------------------
-	
-	dibujarRegistrosParam: function() {
-    	    	/*var out = "";
+    	    	
+    	    	dibujarRegistrosParam: function(arr) {
+    	    	var out = "";
     	    	var i;
-    	    	for(i = 0; i<arr.length; i++) 
+    	    	for(i = 0; i<3; i++) 
     	    	{
     	    		e = arr[i];
-    	    		$('.employee-list').append('<li><img src="img/bibas'+ e.id +'.jpg"><a href=""><span class="titulo" style="width: 100%">' + e.firstName  +'</span><br><span class="categoria" style="width: 100%">'+e.title+'</span><br>350 Bfs<br><span class="categoria" style="width: 100%">' + e.lastName  +'</span></a></li>');
-    	    																	  																	
-    	    	}
-    	    	*/
-    	    	console.log('findAll');
-				$.ajax({
-					type: 'GET',
-					url:  "http://www.flashclicks.com.ve/rest/api/wines/carnes",
-					dataType: "json", // data type of response
-					success: function(data){
-					
-							
-						var list = data == null ? [] : (data.wine instanceof Array ? data.wine : [data.wine]);
-						$.each(list, function(index, wine) {
-						
-							//$('.employee-list').append('<li><img src="img/bibas'+ wine.id +'.jpg"><a href=""><span class="titulo" style="width: 100%">' + wine.name  +'</span><br><span class="categoria" style="width: 100%">'+wine.name+'</span><br>350 Bfs<br><span class="categoria" style="width: 100%">' + wine.name  +'</span></a></li>');
-							$('.employee-list2').append('<li class="precio"><a href="#"><img src="img/bibas.jpg"><h2>'+ wine.name +'</h2><p style="display:none" >150</p><label for="name2" style="color: green;font-size: 15px; ">150 Bsf</label><input type="text" name="name2" id="name2" value="" data-clear-btn="true" hidden="hidden" data-role="none"></a></li>').listview('refresh');
-						
-						
-						});
-	
+    	    		$('.employee-list1').append('<li class="precio"><a href="#"><img src="img/bibas.jpg"><h2>'+ e.firstName +'</h2><p style="display:none" >150</p><label for="name2" style="color: green;font-size: 15px; ">150 Bsf</label><input type="text" name="name2" id="name2" value="" data-clear-btn="true" hidden="hidden" data-role="none"></a></li>');
+															  																	
+    	    	};
+    	    	
+    	    	for(i = 3; i<arr.length; i++) 
+    	    	{
+    	    		e = arr[i];
+    	    		$('.employee-list2').append('<li class="precio"><a href="#"><img src="img/bibas.jpg"><h2>'+ e.firstName +'</h2><p style="display:none" >150</p><label for="name2" style="color: green;font-size: 15px; ">150 Bsf</label><input type="text" name="name2" id="name2" value="" data-clear-btn="true" hidden="hidden" data-role="none"></a></li>');
+															  																	
+    	    	};
 
-					}
-				});
-				
-				$.ajax({
-					type: 'GET',
-					url:  "http://www.flashclicks.com.ve/rest/api/wines/carnes",
-					dataType: "json", // data type of response
-					success: function(data){
-					
-							
-						var list = data == null ? [] : (data.wine instanceof Array ? data.wine : [data.wine]);
-						$.each(list, function(index, wine) {
-						
-							//$('.employee-list').append('<li><img src="img/bibas'+ wine.id +'.jpg"><a href=""><span class="titulo" style="width: 100%">' + wine.name  +'</span><br><span class="categoria" style="width: 100%">'+wine.name+'</span><br>350 Bfs<br><span class="categoria" style="width: 100%">' + wine.name  +'</span></a></li>');
-							//$('.employee-list1').append('<li><a href="#"><img src="img/bibas.jpg"><h2>Google Chrome</h2><p>Google Chrome is a free, open-source web browser. Released in 2008.</p></a></li>');
-							$('.employee-list1').append('<li class="precio"><a href="#"><img src="img/bibas.jpg"><h2>'+ wine.name +'</h2><p style="display:none" >150</p><label for="name2" style="color: green;font-size: 15px; ">150 Bsf</label><input type="text" name="name2" id="name2" value="" data-clear-btn="true" hidden="hidden" data-role="none"></a></li>').listview('refresh');
-						
-						});
-	
+    	    	
+    	    	
+    	    	
 
-					}
-				});
+    	    
+    	    	
+	//},
 				
-				$.ajax({
-					type: 'GET',
-					url:  "http://www.flashclicks.com.ve/rest/api/wines/carnes",
-					dataType: "json", // data type of response
-					success: function(data){
-					
-							
-						var list = data == null ? [] : (data.wine instanceof Array ? data.wine : [data.wine]);
-						$.each(list, function(index, wine) {
-						
-							//$('.employee-list').append('<li><img src="img/bibas'+ wine.id +'.jpg"><a href=""><span class="titulo" style="width: 100%">' + wine.name  +'</span><br><span class="categoria" style="width: 100%">'+wine.name+'</span><br>350 Bfs<br><span class="categoria" style="width: 100%">' + wine.name  +'</span></a></li>');
-							$('.employee-list3').append('<li class="precio"><a href="#"><img src="img/bibas.jpg"><h2>'+ wine.name +'</h2><p style="display:none" >150</p><label for="name2" style="color: green;font-size: 15px; ">150 Bsf</label><input type="text" name="name2" id="name2" value="" data-clear-btn="true" hidden="hidden" data-role="none"></a></li>').listview('refresh');
-						
-						
-						});
-	
-
-					}
-				});
-				
-				$.ajax({
-					type: 'GET',
-					url:  "http://www.flashclicks.com.ve/rest/api/wines/carnes",
-					dataType: "json", // data type of response
-					success: function(data){
-					
-							
-						var list = data == null ? [] : (data.wine instanceof Array ? data.wine : [data.wine]);
-						$.each(list, function(index, wine) {
-						
-							//$('.employee-list').append('<li><img src="img/bibas'+ wine.id +'.jpg"><a href=""><span class="titulo" style="width: 100%">' + wine.name  +'</span><br><span class="categoria" style="width: 100%">'+wine.name+'</span><br>350 Bfs<br><span class="categoria" style="width: 100%">' + wine.name  +'</span></a></li>');
-							$('.employee-list4').append('<li class="precio"><a href="#"><img src="img/bibas.jpg"><h2>'+ wine.name +'</h2><p style="display:none" >150</p><label for="name2" style="color: green;font-size: 15px; ">150 Bsf</label><input type="text" name="name2" id="name2" value="" data-clear-btn="true" hidden="hidden" data-role="none"></a></li>').listview('refresh');
-						
-						
-						});
-	
-
-					}
-				});
-				
-				$.ajax({
+				/*$.ajax({
 					type: 'GET',
 					url:  "http://www.flashclicks.com.ve/rest/api/wines/carnes",
 					dataType: "json", // data type of response
@@ -139,7 +46,7 @@ var app = {
 	
 
 					}
-				});
+				});*/
 
 
 
@@ -158,7 +65,9 @@ var app = {
     initialize: function() {
     	this.homeTpl = Handlebars.compile($("#home-tpl").html());
     	this.renderHomeView();    	
-    	this.dibujarRegistrosParam();
+    	this.dibujarRegistrosParam(registrosJson);
+
+
     	//findAll();
 	}
 };
