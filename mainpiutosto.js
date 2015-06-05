ddb6b09dd2278b74ad1888d7cfc4e8c8edc94242
paulmoody <moodyweb@hotmail.com>
2015-06-05 16:47:51 -0430

    precios piutosto

--- mainpiutosto.js
+++ mainpiutosto.js
@@ -6,48 +6,38 @@ var app = {
     	    	dibujarRegistrosParam: function(arr) {
     	    	var out = "";
     	    	var i;
-    	    	for(i = 0; i<3; i++) 
+    	    	for(i = 0; i<1; i++) 
     	    	{
     	    		e = arr[i];
     	    		$('.employee-list1').append('<li class="precio"><a href="#"><img src="img/bibas.jpg"><h2>'+ e.firstName +'</h2><p style="display:none" >'+e.precio+'</p><label for="name2" style="color: green;font-size: 15px; ">'+e.precio+' Bsf</label><input type="text" name="name2" id="name2" value="" data-clear-btn="true" hidden="hidden" data-role="none"></a></li>');
 															  																	
     	    	};
     	    	
-    	    	for(i = 3; i<5; i++) 
+    	    	for(i = 1; i<5; i++) 
     	    	{
     	    		e = arr[i];
-    	    		$('.employee-list2').append('<li class="precio"><a href="#"><img src="img/bibas.jpg"><h2>'+ e.firstName +'</h2><p style="display:none" >150</p><label for="name2" style="color: green;font-size: 15px; ">150 Bsfasd</label><input type="text" name="name2" id="name2" value="" data-clear-btn="true" hidden="hidden" data-role="none"></a></li>');
+    	    		$('.employee-list2').append('<li class="precio"><a href="#"><img src="img/bibas.jpg"><h2>'+ e.firstName +'</h2><p style="display:none" >'+e.precio+'</p><label for="name2" style="color: green;font-size: 15px; ">'+e.precio+' Bsf</label><input type="text" name="name2" id="name2" value="" data-clear-btn="true" hidden="hidden" data-role="none"></a></li>');
 															  																	
     	    	};
     	    	
-    	    	for(i = 5; i<7; i++) 
+    	    	for(i = 5; i<8; i++) 
     	    	{
     	    		e = arr[i];
-    	    		$('.employee-list3').append('<li class="precio"><a href="#"><img src="img/bibas.jpg"><h2>'+ e.firstName +'</h2><p style="display:none" >150</p><label for="name2" style="color: green;font-size: 15px; ">150 Bsf</label><input type="text" name="name2" id="name2" value="" data-clear-btn="true" hidden="hidden" data-role="none"></a></li>');
+    	    		$('.employee-list3').append('<li class="precio"><a href="#"><img src="img/bibas.jpg"><h2>'+ e.firstName +'</h2><p style="display:none" >'+e.precio+'</p><label for="name2" style="color: green;font-size: 15px; ">'+e.precio+' Bsf</label><input type="text" name="name2" id="name2" value="" data-clear-btn="true" hidden="hidden" data-role="none"></a></li>');
 															  																	
     	    	};
     	    	
-    	    	for(i = 7; i<arr.length; i++) 
+    	    	for(i = 8; i<arr.length; i++) 
     	    	{
     	    		e = arr[i];
-    	    		$('.employee-list4').append('<li class="precio"><a href="#"><img src="img/bibas.jpg"><h2>'+ e.firstName +'</h2><p style="display:none" >150</p><label for="name2" style="color: green;font-size: 15px; ">150 Bsf</label><input type="text" name="name2" id="name2" value="" data-clear-btn="true" hidden="hidden" data-role="none"></a></li>');
+    	    		$('.employee-list4').append('<li class="precio"><a href="#"><img src="img/bibas.jpg"><h2>'+ e.firstName +'</h2><p style="display:none" >'+e.precio+'</p><label for="name2" style="color: green;font-size: 15px; ">'+e.precio+' Bsf</label><input type="text" name="name2" id="name2" value="" data-clear-btn="true" hidden="hidden" data-role="none"></a></li>');
 															  																	
     	    	};
     	    	
-    	    	for(i = 9; i<arr.length; i++) 
-    	    	{
-    	    		e = arr[i];
-    	    		$('.employee-list5').append('<li class="precio"><a href="#"><img src="img/bibas.jpg"><h2>'+ e.firstName +'</h2><p style="display:none" >150</p><label for="name2" style="color: green;font-size: 15px; ">150 Bsf</label><input type="text" name="name2" id="name2" value="" data-clear-btn="true" hidden="hidden" data-role="none"></a></li>');
-															  																	
-    	    	};
-
-
-
-
+    	    
     	    	
     	    	
-    	    	
-
+  
     	    
     	    	
 	//},
